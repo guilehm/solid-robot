3008e138dcf43c4727490a0491e30057613e2d7e
gui <guihm.dev@gmail.com>
2023-06-17 17:39:41 -0300

    minor updates

--- internal/storages/postgres/postgres.go
+++ internal/storages/postgres/postgres.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
-	"os"
 	"time"
 )
 
@@ -17,8 +16,9 @@ func NewPostgresStorage() *Postgres {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	// db, err := pgxpool.New(ctx, "postgres://postgres:postgres@localhost:5432/dataloader")
-	db, err := pgxpool.New(ctx, os.Getenv("DATABASE_URL"))
+	db, err := pgxpool.New(ctx, "postgres://postgres:postgres@localhost:5432/solid")
+
+	// db, err := pgxpool.New(ctx, os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("unable to connect to database: %v", err.Error())
 	}
