66cf21f8201f3b36b7e2203aa745d2743e879b0c
gui <guihm.dev@gmail.com>
2023-06-17 10:13:12 -0300

    create constants

---
+++ internal/services/constants.go
@@ -0,0 +1,20 @@
+package services
+
+const (
+	DocumentIndexStart           = 0
+	DocumentIndexEnd             = 19
+	PrivateIndexStart            = 19
+	PrivateIndexEnd              = 31
+	IncompleteIndexStart         = 31
+	IncompleteIndexEnd           = 43
+	LastPurchaseDateIndexStart   = 43
+	LastPurchaseDateIndexEnd     = 65
+	TicketAverageIndexStart      = 65
+	TicketAverageIndexEnd        = 87
+	TicketLastPurchaseIndexStart = 87
+	TicketLastPurchaseIndexEnd   = 111
+	StoreMostFrequentIndexStart  = 111
+	StoreMostFrequentIndexEnd    = 131
+	StoreLastPurchaseIndexStart  = 131
+	StoreLastPurchaseIndexEnd    = -1
+)
