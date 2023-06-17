2c2e75f11d25c1e8a12b69e92194f0f933fb04ab
gui <guihm.dev@gmail.com>
2023-06-17 17:34:19 -0300

    add update client raw

---
+++ internal/services/updateClientRaw.go
@@ -0,0 +1,34 @@
+package services
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/guilehm/solid-robot/internal/features/models"
+	clientRawRepository "github.com/guilehm/solid-robot/internal/features/repository/clientRaw"
+	"github.com/jackc/pgx/v5"
+)
+
+type clientRawUpdateInput struct {
+	ID     uuid.UUID
+	Status models.ClientRawStatus
+}
+
+func (service *ServiceGroup) updateClientRaw(ctx context.Context, batch *pgx.Batch, channel <-chan clientRawUpdateInput) {
+
+	for clientRaw := range channel {
+		if batch.Len() >= service.bulkAmount {
+			service.commands.SendUpdateBatch(ctx, batch)
+
+			newBatch := &pgx.Batch{}
+			*batch = *newBatch
+		}
+		clientRawRepository.UpdateBatch(batch, string(clientRaw.Status), clientRaw.ID)
+	}
+
+	if batch.Len() > 0 {
+		service.commands.SendUpdateBatch(ctx, batch)
+	}
+
+	service.logger.Info().Msg("finished updating client raw")
+
+}
