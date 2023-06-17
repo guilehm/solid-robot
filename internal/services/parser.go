e6b92fdf44d18c8fa8ecf8ac94f72c5c67c3b862
gui <guihm.dev@gmail.com>
2023-06-17 10:13:07 -0300

    create parser

--- internal/services/parser.go
+++ internal/services/parser.go
@@ -1,10 +1,35 @@
 package services
 
-import "fmt"
+import (
+	"github.com/google/uuid"
+	"github.com/guilehm/solid-robot/internal/features/models"
+	"strings"
+	"time"
+)
+
+func getValue(start, end int, line string) string {
+	if end == -1 {
+		return strings.Trim(line[start:], " ")
+	}
+	return strings.Trim(line[start:end], " ")
+}
 
 func (service *ServiceGroup) parser() {
 	for {
-		line := <-service.rawMsgChannel
-		fmt.Println("received", len(line))
+		line := <-service.channelRawMsg
+
+		service.channelClientRaw <- models.ClientRaw{
+			ID:                 uuid.NewString(),
+			Document:           getValue(DocumentIndexStart, DocumentIndexEnd, line),
+			Private:            getValue(PrivateIndexStart, PrivateIndexEnd, line),
+			Incomplete:         getValue(IncompleteIndexStart, IncompleteIndexEnd, line),
+			LastPurchaseDate:   getValue(LastPurchaseDateIndexStart, LastPurchaseDateIndexEnd, line),
+			TicketAverage:      getValue(TicketAverageIndexStart, TicketAverageIndexEnd, line),
+			TicketLastPurchase: getValue(TicketLastPurchaseIndexStart, TicketLastPurchaseIndexEnd, line),
+			StoreMostFrequent:  getValue(StoreMostFrequentIndexStart, StoreMostFrequentIndexEnd, line),
+			StoreLastPurchase:  getValue(StoreLastPurchaseIndexStart, StoreLastPurchaseIndexEnd, line),
+			// Status:             "",
+			CreatedAt: time.Now().Format(time.RFC3339),
+		}
 	}
 }
