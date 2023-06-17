bdedcc294767cddecd2fd64e1fd0818616809969
gui <guihm.dev@gmail.com>
2023-06-17 11:10:21 -0300

    create command to insert client raw data

---
+++ internal/features/commands/createClientRaw.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/guilehm/solid-robot/internal/features/models"
+	clientRawRepository "github.com/guilehm/solid-robot/internal/features/repository/clientRaw"
+)
+
+func (cg *CommandGroup) CreateClientRaw(ctx context.Context, clientRaw models.ClientRaw) (uuid.UUID, error) {
+	logger := cg.logger.
+		With().
+		Str("id", clientRaw.ID.String()).
+		Logger()
+
+	logger.Debug().Msg("creating client raw")
+
+	id, err := clientRawRepository.Create(ctx, cg.postgres.DB, clientRaw)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	logger.Debug().Msg("client raw successfully created")
+	return id, nil
+}
