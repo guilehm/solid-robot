a658cd47eea73732436bed27107c90fd66a52d9a
gui <guihm.dev@gmail.com>
2023-06-19 21:35:39 -0300

    increase performance a lot

--- internal/services/parserWorker.go
+++ internal/services/parserWorker.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/google/uuid"
 	"github.com/guilehm/solid-robot/internal/features/models"
+	"runtime"
 	"strings"
 	"time"
 )
@@ -16,24 +17,31 @@ func getValue(start, end int, line string) string {
 }
 
 func (service *ServiceGroup) parserWorker(ctx context.Context, rawMsgChannel <-chan string, channelClientRaw chan<- models.ClientRaw) {
-	for line := range rawMsgChannel {
-		now := time.Now().Format(time.RFC3339)
-		channelClientRaw <- models.ClientRaw{
-			ID:                 uuid.New(),
-			Document:           getValue(DocumentIndexStart, DocumentIndexEnd, line),
-			Private:            getValue(PrivateIndexStart, PrivateIndexEnd, line),
-			Incomplete:         getValue(IncompleteIndexStart, IncompleteIndexEnd, line),
-			LastPurchaseDate:   getValue(LastPurchaseDateIndexStart, LastPurchaseDateIndexEnd, line),
-			TicketAverage:      getValue(TicketAverageIndexStart, TicketAverageIndexEnd, line),
-			TicketLastPurchase: getValue(TicketLastPurchaseIndexStart, TicketLastPurchaseIndexEnd, line),
-			StoreMostFrequent:  getValue(StoreMostFrequentIndexStart, StoreMostFrequentIndexEnd, line),
-			StoreLastPurchase:  getValue(StoreLastPurchaseIndexStart, StoreLastPurchaseIndexEnd, line),
-			Status:             models.ClientRawStatusWaiting,
-			CreatedAt:          now,
-			UpdatedAt:          now,
+	done := make(chan bool, runtime.NumCPU())
+	for i := 0; i < runtime.NumCPU(); i++ {
+		for line := range rawMsgChannel {
+			now := time.Now().Format(time.RFC3339)
+			channelClientRaw <- models.ClientRaw{
+				ID:                 uuid.New(),
+				Document:           getValue(DocumentIndexStart, DocumentIndexEnd, line),
+				Private:            getValue(PrivateIndexStart, PrivateIndexEnd, line),
+				Incomplete:         getValue(IncompleteIndexStart, IncompleteIndexEnd, line),
+				LastPurchaseDate:   getValue(LastPurchaseDateIndexStart, LastPurchaseDateIndexEnd, line),
+				TicketAverage:      getValue(TicketAverageIndexStart, TicketAverageIndexEnd, line),
+				TicketLastPurchase: getValue(TicketLastPurchaseIndexStart, TicketLastPurchaseIndexEnd, line),
+				StoreMostFrequent:  getValue(StoreMostFrequentIndexStart, StoreMostFrequentIndexEnd, line),
+				StoreLastPurchase:  getValue(StoreLastPurchaseIndexStart, StoreLastPurchaseIndexEnd, line),
+				Status:             models.ClientRawStatusWaiting,
+				CreatedAt:          now,
+				UpdatedAt:          now,
+			}
 		}
+		done <- true
 	}
 
+	for i := 0; i < runtime.NumCPU(); i++ {
+		<-done
+	}
 	close(channelClientRaw)
 
 	service.logger.Info().Msg("finished parsing data")
